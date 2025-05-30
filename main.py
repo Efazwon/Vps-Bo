d97845fc9312d06b61d5c06429512391d51de44e
Efazwon <raishadhabibefaz2024@gmail.com>
2025-05-30 13:25:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-############################ Made By Foxytouxxx ############################
-# Do not copy, modify, merge, publish, distribute, sublicense, and/or sell copies of this script
-
 import discord
 from discord.ext import commands
 import subprocess
 import time
 import asyncio
+import os  # To read environment variables
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,7 +15,7 @@ start_time = time.time()
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    await update_status()
+    asyncio.create_task(update_status())
 
 @bot.command(name='exe')
 async def execute_command(ctx, *, command):
@@ -25,11 +23,9 @@ async def execute_command(ctx, *, command):
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         output = result.stdout
 
-        # Check the length of the output
         if len(output) <= 1950:
             await ctx.send(f'Command executed successfully:\n```\n{output}\n```')
         else:
-            # Split the output into chunks of 1950 characters
             for i in range(0, len(output), 1950):
                 await ctx.send(f'Command executed successfully (Part {i//1950 + 1}):\n```\n{output[i:i+1950]}\n```')
 
@@ -38,19 +34,18 @@ async def execute_command(ctx, *, command):
 
 async def update_status():
     while True:
-        # Calculate the uptime in seconds
         uptime_seconds = int(time.time() - start_time)
-        # Makes it a bit more readable
         uptime_formatted = time.strftime('%H:%M:%S', time.gmtime(uptime_seconds))
-      
         await bot.change_presence(activity=discord.Game(name=f'Uptime: {uptime_formatted}'))
-      
-        # Update your status every 60 seconds
         await asyncio.sleep(60)
 
 async def main():
-    asyncio.create_task(update_status())
-    await bot.start('MTM3Nzg5ODgzMTU2MzMyNTQ2MQ.GjGoM4.4Oubh3RoyQgsRgaPlbeghln5AugfL40zcBqCok')
+    token = os.getenv('DISCORD_TOKEN')
+    if not token:
+        print("ERROR: DISCORD_TOKEN environment variable not set.")
+        return
+    await bot.start(token)
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
